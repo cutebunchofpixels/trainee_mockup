e1ae0be82bf0fedbf41e63c189fe9a2312b55120
cutebunchofpixels <poroshkinanthony18@gmail.com>
2024-03-04 15:22:08 +0200

    remove cra boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,8 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
-import { Button } from 'antd'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Button>Learn React</Button>
-      </header>
-    </div>
-  )
+  return <div></div>
 }
 
 export default App
