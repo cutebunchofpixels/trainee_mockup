2daa04cf93981c75db17a6bec054203dc35d8da2
cutebunchofpixels <poroshkinanthony18@gmail.com>
2024-03-13 09:33:39 +0200

    refactor: move theme switch logic into corresponding component

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,10 @@ import variables from './sass/abstracts/_variables.scss'
 import './styles.module.scss'
 import ThemeSwitch from 'components/layout/ThemeSwitch'
 import { Theme } from 'types/Theme'
-import { useAppDispatch, useAppSelector } from 'redux/app/hooks'
-import { setTheme } from 'redux/actions/theme'
-import { ls } from 'utils/secureLS'
-import { PREFERED_THEME_KEY } from 'utils/constants'
+import { useAppSelector } from 'redux/app/hooks'
 
 function App() {
   const { value: currentTheme } = useAppSelector(state => state.theme)
-  const dispatch = useAppDispatch()
 
   return (
     <ConfigProvider
@@ -30,15 +26,7 @@ function App() {
     >
       <Layout>
         <Layout.Header>
-          <ThemeSwitch
-            currentTheme={currentTheme}
-            onChange={() => {
-              const newTheme =
-                currentTheme === Theme.Dark ? Theme.Light : Theme.Dark
-              dispatch(setTheme(newTheme))
-              ls.set(PREFERED_THEME_KEY, newTheme)
-            }}
-          />
+          <ThemeSwitch />
         </Layout.Header>
         <Layout.Content>
           <Home />
