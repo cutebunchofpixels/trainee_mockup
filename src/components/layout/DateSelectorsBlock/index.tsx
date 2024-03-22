dbe5b33ef098e9005b3b1a870d4ff9ba9a45ce35
cutebunchofpixels <poroshkinanthony18@gmail.com>
2024-03-22 14:47:20 +0200

    feat: add interactivity to date selector form

--- src/components/layout/DateSelectorsBlock/index.tsx
+++ src/components/layout/DateSelectorsBlock/index.tsx
@@ -1,31 +1,100 @@
 import React from 'react'
 import { LeftOutlined, RightOutlined } from '@ant-design/icons'
-import { Button, Card, Col, Row } from 'antd'
+import { Button, Card, Form, theme } from 'antd'
 import { useTranslation } from 'react-i18next'
+import dayjs, { Dayjs } from 'dayjs'
+import { useMediaQuery } from 'react-responsive'
 
 import DateSelector from 'src/components/ui/DateSelector'
 
+import styles from './styles.module.scss'
+
+interface FormValues {
+  startDate: Dayjs
+  endDate: Dayjs
+}
+
 export default function DateSelectorsBlock() {
   const { t, i18n } = useTranslation()
+  const [form] = Form.useForm<FormValues>()
+  const { token } = theme.useToken()
+  const isScreenMd = useMediaQuery({ maxWidth: token.screenMD })
 
   return (
     <Card>
-      <form>
-        <Row gutter={[32, 16]}>
-          <Col xs={24} md={9}>
-            <DateSelector caption={t('reportDateSelectForm.startDate')} />
-          </Col>
-          <Col xs={24} md={9}>
-            <DateSelector caption={t('reportDateSelectForm.endDate')} />
-          </Col>
-          <Col xs={24} md={{ span: 5, offset: 1 }}>
-            <Button type="primary" htmlType="submit">
-              {t('reportDateSelectForm.viewReport')}
-              {i18n.dir() === 'ltr' ? <RightOutlined /> : <LeftOutlined />}
-            </Button>
-          </Col>
-        </Row>
-      </form>
+      <Form
+        layout={isScreenMd ? 'vertical' : 'inline'}
+        className={styles.dateSelectorsForm}
+        onFinish={() => {
+          console.log('submit')
+        }}
+        onFinishFailed={() => {
+          console.log(form.getFieldsError())
+          console.log('error')
+        }}
+        form={form}
+      >
+        <Form.Item<FormValues>
+          name="startDate"
+          label={t('reportDateSelectForm.startDate')}
+          rules={[{ required: true }]}
+        >
+          <DateSelector />
+        </Form.Item>
+        <Form.Item<FormValues>
+          name="endDate"
+          label={t('reportDateSelectForm.endDate')}
+          rules={[
+            { required: true },
+            {
+              validator: (_, value: Dayjs) => {
+                const startDate = form.getFieldValue('startDate')
+
+                if (!startDate) {
+                  return Promise.resolve()
+                }
+
+                if (value && !value.isBefore(startDate)) {
+                  return Promise.resolve()
+                }
+
+                return Promise.reject()
+              },
+              message: 'End date must not be earlier than start date',
+            },
+            {
+              validator: (_, value: Dayjs) => {
+                const startDate = form.getFieldValue('startDate')
+
+                if (value && value.diff(startDate, 'day') > 5) {
+                  return Promise.reject()
+                }
+
+                return Promise.resolve()
+              },
+              message: 'Selected period cannot be longer than 5 days',
+            },
+            {
+              validator: (_, value: Dayjs) => {
+                if (value && value.isAfter(dayjs())) {
+                  return Promise.reject()
+                }
+
+                return Promise.resolve()
+              },
+              message: 'Selected period cannot be longer than 5 days',
+            },
+          ]}
+        >
+          <DateSelector />
+        </Form.Item>
+        <Form.Item noStyle>
+          <Button type="primary" htmlType="submit">
+            {t('reportDateSelectForm.viewReport')}
+            {i18n.dir() === 'ltr' ? <RightOutlined /> : <LeftOutlined />}
+          </Button>
+        </Form.Item>
+      </Form>
     </Card>
   )
 }
