045cdcde6516cd2ffdf6d1bbed28ea75443ee47b
cutebunchofpixels <poroshkinanthony18@gmail.com>
2024-03-25 14:32:13 +0200

    feat: current user section

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -1,45 +1,18 @@
 import React from 'react'
-import { Layout, Button, message } from 'antd'
-import { signOut } from 'firebase/auth'
-import { FirebaseError } from 'firebase/app'
-import { useNavigate } from 'react-router-dom'
-import { useTranslation } from 'react-i18next'
+import { Layout } from 'antd'
 
 import ThemeSwitch from '../ThemeSwitch'
 import LocaleSelect from '../LocaleSelect'
-import { auth } from 'fb'
-import { useAppSelector } from 'redux/app/hooks'
+import CurrentUserSection from '../CurrentUserSection'
 
 import styles from './styles.module.scss'
 
 export default function Header() {
-  const [messageApi, contextHolder] = message.useMessage()
-  const navigate = useNavigate()
-  const user = useAppSelector(state => state.auth.user)
-  const { t } = useTranslation()
-
-  function handleSignout() {
-    signOut(auth)
-      .then(() => navigate('/signin'))
-      .catch(error => {
-        if (error instanceof FirebaseError) {
-          messageApi.error(error.message)
-        } else {
-          messageApi.error('Unexpected error occured')
-        }
-      })
-  }
-
   return (
     <Layout.Header className={styles.header}>
-      {contextHolder}
       <ThemeSwitch />
+      <CurrentUserSection />
       <LocaleSelect />
-      {user && (
-        <Button type="primary" onClick={handleSignout}>
-          {t('auth.signout')}
-        </Button>
-      )}
     </Layout.Header>
   )
 }
