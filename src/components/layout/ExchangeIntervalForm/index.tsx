ecefc06852357ade67f83ab1a6f5e7c372f9908a
cutebunchofpixels <poroshkinanthony18@gmail.com>
2024-03-26 12:57:26 +0200

    fix: disable invalid dates in date selector of exchange interval form insted of showing validation error messages

--- src/components/layout/ExchangeIntervalForm/index.tsx
+++ src/components/layout/ExchangeIntervalForm/index.tsx
@@ -7,7 +7,6 @@ import { dayjs } from 'src/utils/dayjs'
 import { useMediaQuery } from 'react-responsive'
 
 import DateSelector from 'src/components/ui/DateSelector'
-import { minDate, dateDifference } from 'src/utils/validators'
 import { useAppDispatch, useAppSelector } from 'src/redux/app/hooks'
 import { fetchExchangeRates } from 'src/redux/thunks/currencyExchange'
 import { Currency } from 'src/types/Currency'
@@ -49,15 +48,23 @@ function shouldRefetch(
   return false
 }
 
+const MIN_INTERVAL_SIZE = 3
+const MAX_INTERVAL_SIZE = 5
+
 export default function ExchangeIntervalForm() {
   const [form] = Form.useForm<FormValues>()
+  const { t, i18n } = useTranslation()
+
   const { token } = theme.useToken()
   const isScreenMd = useMediaQuery({ maxWidth: token.screenMD })
+
   const dispatch = useAppDispatch()
-  const { t, i18n } = useTranslation()
   const currencyExchangeRates = useAppSelector(
     state => state.currencyExchange.data
   )
+
+  const startDate = Form.useWatch('startDate', form)
+  const endDate = Form.useWatch('endDate', form)
 
   return (
     <Form<FormValues>
@@ -74,52 +81,64 @@ export default function ExchangeIntervalForm() {
       <Form.Item<FormValues>
         name="startDate"
         label={t('reportDateSelectForm.startDate')}
+        help={''}
         rules={[
           {
             required: true,
-            message: t('errors.dateSelectors.startDateRequired'),
           },
         ]}
       >
-        <DateSelector />
+        <DateSelector
+          disabledDate={date => {
+            if (!endDate && date.isBefore(dayjs())) {
+              return false
+            }
+
+            if (
+              endDate &&
+              endDate.diff(date, 'day') >= MIN_INTERVAL_SIZE &&
+              endDate.diff(date, 'day') <= MAX_INTERVAL_SIZE
+            ) {
+              return false
+            }
+
+            return true
+          }}
+        />
       </Form.Item>
       <Form.Item<FormValues>
-        name="endDate"
         label={t('reportDateSelectForm.endDate')}
+        name="endDate"
         rules={[
           {
             required: true,
-            message: t('errors.dateSelectors.endDateRequired'),
-          },
-          {
-            validator: (_, endDate: Dayjs) =>
-              minDate(form.getFieldValue('startDate'), endDate),
-            message: t('errors.dateSelectors.endDateBeforeStartDate'),
-          },
-          {
-            validator: (_, endDate: Dayjs) =>
-              dateDifference(endDate, form.getFieldValue('startDate'), {
-                maxDifference: { value: 5, unit: 'day' },
-              }),
-            message: t('errors.dateSelectors.selectedPeriodIsTooLong'),
-          },
-          {
-            validator: (_, endDate: Dayjs) =>
-              dateDifference(endDate, form.getFieldValue('startDate'), {
-                minDifference: { value: 3, unit: 'day' },
-              }),
-            message: t('errors.dateSelectors.selectedPeriodIsTooShort'),
-          },
-          {
-            validator: (_, endDate: Dayjs) => minDate(endDate, dayjs()),
-            message: t('errors.dateSelectors.futureEndDate'),
           },
         ]}
       >
-        <DateSelector />
+        <DateSelector
+          disabledDate={date => {
+            if (!startDate && date.isBefore(dayjs())) {
+              return false
+            }
+
+            if (
+              startDate &&
+              date.diff(startDate, 'day') >= MIN_INTERVAL_SIZE &&
+              date.diff(startDate, 'day') <= MAX_INTERVAL_SIZE
+            ) {
+              return false
+            }
+
+            return true
+          }}
+        />
       </Form.Item>
       <Form.Item noStyle>
-        <Button type="primary" htmlType="submit">
+        <Button
+          type="primary"
+          htmlType="submit"
+          disabled={!startDate || !endDate}
+        >
           {t('reportDateSelectForm.viewReport')}
           {i18n.dir() === 'ltr' ? <RightOutlined /> : <LeftOutlined />}
         </Button>
