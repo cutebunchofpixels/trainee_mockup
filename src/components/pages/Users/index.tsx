bd4d92ed77678a494690f321eb0b40f4f3b6ebdc
cutebunchofpixels <poroshkinanthony18@gmail.com>
2024-04-02 17:10:22 +0300

    fix: add Hebrew translation for users page heading

--- src/components/pages/Users/index.tsx
+++ src/components/pages/Users/index.tsx
@@ -5,12 +5,14 @@ import UsersTable from 'src/components/layout/UsersTable'
 import UsersFilters from 'src/components/layout/UsersFilters'
 import { useAppDispatch, useAppSelector } from 'src/redux/app/hooks'
 import { fetchUsers } from 'src/redux/thunks/users'
+import { useTranslation } from 'react-i18next'
 
 import styles from './styles.module.scss'
 
 export default function Users() {
   const users = useAppSelector(state => state.users.data)
   const dispatch = useAppDispatch()
+  const { t } = useTranslation()
 
   useEffect(() => {
     if (users.length === 0) {
@@ -20,7 +22,9 @@ export default function Users() {
 
   return (
     <div className={styles.usersPageContainer}>
-      <Typography.Title className={styles.pageHeading}>Users</Typography.Title>
+      <Typography.Title className={styles.pageHeading}>
+        {t('users.pageHeading')}
+      </Typography.Title>
       <UsersFilters />
       <UsersTable />
     </div>
